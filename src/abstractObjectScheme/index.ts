732a68a3d97f10d6a08d9afec8e60068018dd17a
LeoVS09 <leovs010@gmail.com>
2019-11-24 19:40:21 +0300

    add jsog ideas

--- src/abstractObjectScheme/index.ts
+++ src/abstractObjectScheme/index.ts
@@ -1,2 +1,7 @@
 export * from './types'
 export * from './utils'
+
+// TODO: may be use graph representation of object scheme
+// TODO: may be use for database store graph representation
+// TODO: try to implement this with JSOG: https://github.com/jsog/jsog
+// and typescript implementation https://github.com/emundo/jsog-typescript
