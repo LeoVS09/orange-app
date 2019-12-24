ff8f77b6468d4b7c716de8b5d61be2853572c84b
LeoVS09 <leovs09@gmail.com>
2019-12-25 02:25:34 +0300

    lint fix

--- src/lazyDB/database/cycle/utils.ts
+++ src/lazyDB/database/cycle/utils.ts
@@ -3,10 +3,10 @@ import { sample, skipWhile, takeWhile } from 'rxjs/operators'
 
 // TODO: turn into operator
 export function takeWhileThenContinue<T>(
-  stream: Observable<T>, 
+  stream: Observable<T>,
   predicate: (v: T) => boolean,
-   sampler: Observable<any>
-   ) {
+  sampler: Observable<any>,
+) {
   const whileTrue = stream.pipe(takeWhile(predicate))
   const lastWhenFalse = stream.pipe(
     skipWhile(predicate),
