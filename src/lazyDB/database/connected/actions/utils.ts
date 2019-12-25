ac5edc6170eb4664c0d1342d44a072926f6707e6
LeoVS09 <leovs09@gmail.com>
2019-12-25 23:44:46 +0300

    eslint fix

--- src/lazyDB/database/connected/actions/utils.ts
+++ src/lazyDB/database/connected/actions/utils.ts
@@ -3,10 +3,10 @@ import { ModelEventGetPropertyPayload } from '@/lazyDB/core/types'
 import { lastObjectPropertyName } from '../../utils'
 
 export function isExcludeProperty({ excludeProperties }: IDatabaseModelProducerStore, payload: ModelEventGetPropertyPayload) {
-    if (!excludeProperties)
-        return false
+  if (!excludeProperties)
+    return false
 
-    const prop = lastObjectPropertyName(payload)
+  const prop = lastObjectPropertyName(payload)
 
-    return excludeProperties.includes(prop)
+  return excludeProperties.includes(prop)
 }
