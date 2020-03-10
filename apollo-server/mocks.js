5f3ed17b6b3bb1245a8e0d62131aa67f973f4b0f
LeoVS09 <leovs09@gmail.com>
2020-03-11 00:50:03 +0300

    add list mocking

--- apollo-server/mocks.js
+++ apollo-server/mocks.js
@@ -2,10 +2,20 @@
 // Customize mocking: https://www.apollographql.com/docs/graphql-tools/mocking.html#Customizing-mocks
 import shortid from 'shortid'
 import casual from 'casual'
+import { MockList } from 'vue-cli-plugin-apollo/node_modules/graphql-tools'
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+const mockConnection = () => ({
+  nodes: () => new MockList([5, 20])
+})
 
 export default {
   UUID: () => shortid.generate(),
   Datetime: () => new Date().toISOString(),
+  Int: () => getRandomInt(20) + 5,
 
   Country: () => ({
     name: casual.country
@@ -29,5 +39,15 @@ export default {
     firstName: casual.first_name,
     middleName: casual.last_name,
     lastName: casual.last_name
+  }),
+
+  Tag: () => ({
+    name: casual.word
+  }),
+
+  Query: () => ({
+    countries: mockConnection,
+    tags: mockConnection,
+    problems: mockConnection
   })
 }
