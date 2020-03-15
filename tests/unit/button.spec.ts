051c5cc676564a7158dcf33d4dd31ef0689f6581
LeoVS09 <leovs09@gmail.com>
2020-03-15 19:58:31 +0300

    setup snapshot testing

--- tests/unit/button.spec.ts
+++ tests/unit/button.spec.ts
@@ -3,10 +3,11 @@ import Button from '@/components/Button.vue'
 
 describe('Button.vue', () => {
   it('renders slot when passed', () => {
-    const msg = 'new message'
+    const msg = 'Click on me'
     const wrapper = shallowMount(Button, {
       slots: { default: msg }
     })
     expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
