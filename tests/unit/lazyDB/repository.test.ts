686a4296cee8d7fc0ff98a02972dd137b7a57a54
LeoVS09 <leovs09@gmail.com>
2020-03-20 00:42:53 +0300

    add repository list test

---
+++ tests/unit/lazyDB/repository.test.ts
@@ -0,0 +1,75 @@
+// @ts-ignore next-line
+global.fetch = require('node-fetch')
+import nock from 'nock'
+import { nockAllGraphqlRequests } from '../utils/graphql.mock'
+import cons from '../utils/console.mock'
+import flushPromises from 'flush-promises'
+import { timeout } from '../utils/timeout'
+
+describe('Repository', () => {
+  beforeAll(() => nock.disableNetConnect());
+
+  beforeEach(async () => {
+    await nockAllGraphqlRequests(nock, 'repository')
+    cons.mockConsole()
+  })
+
+  afterEach(() => {
+    cons.restoreConsole()
+    nock.cleanAll()
+  });
+
+  it('generade query for list and return', async () => {
+    // must be inside test for mock fetch function
+    const Repository = require('@/models/country').CountryRepository
+  
+    const reactiveUpdate = jest.fn()
+    const list = Repository.list(reactiveUpdate)
+
+    // ask data
+    expect(list.nodes[0].name).toBeUndefined()
+    expect(list.nodes[0].code).toBeUndefined()
+    expect(list.nodes[0].updatedAt).toBeUndefined()
+    
+    // Wait while lazyDB requested all data and rerender
+    await timeout(300)
+    await flushPromises()
+
+    const resultedList = Repository.list(reactiveUpdate)
+
+    // Currently only 1 times call update 
+    // TODO: investigate why
+    //expect(reactiveUpdate.mock.calls.length).toBe(2)
+
+    expect(resultedList.nodes.length).toBe(15)
+
+    expect(resultedList.nodes[0].id).toBe("gILNkgtqw")
+    expect(resultedList.nodes[0].name).toBe("Estonia")
+    expect(resultedList.nodes[0].code).toBe("DE")
+    expect(resultedList.nodes[0].updatedAt).toEqual(new Date("2020-02-27T00:25:04+03:00"))
+
+    expect(resultedList.nodes[6].id).toBe("vP38-NdB-S")
+    expect(resultedList.nodes[6].name).toBe("Guinea-Bissau")
+    expect(resultedList.nodes[6].code).toBe("IE")
+    expect(resultedList.nodes[6].updatedAt).toEqual(new Date("2020-03-15T00:25:04+03:00"))
+
+    // TODO: equility comparisition for lazy objects not work, need fix
+    // expect(resultedList.nodes).toEqual([
+    //   {"id":"gILNkgtqw","nodeId":"e594829f-9e24-42ca-9743-a981f89f1583","name":"Estonia","code":"DE","updatedAt":"2020-02-27T00:25:04+03:00"},
+    //   {"id":"RvBE9BWZ85","nodeId":"f4793d49-1da5-4cb1-b5ef-85e8d244d56b","name":"Comoros","code":"CN","updatedAt":"2020-03-02T00:25:04+03:00"},
+    //   {"id":"0Pj8HSsj2e","nodeId":"4ef01f57-260d-40ae-a924-a97838b13b58","name":"Mayotte","code":"IN","updatedAt":"2020-03-07T00:25:04+03:00"},
+    //   {"id":"mIBSPRsCW1","nodeId":"7fd05815-6b7d-4cae-9b8c-3ab4ca94e870","name":"Kuwait","code":"US","updatedAt":"2020-03-04T00:25:04+03:00"},
+    //   {"id":"hcQnOCkpmJ","nodeId":"f978be87-e3d8-4bea-88bd-e2c3eb8c5a5e","name":"Slovakia (Slovak Republic)","code":"US","updatedAt":"2020-03-07T00:25:04+03:00"},
+    //   {"id":"N8RK5pKUpR","nodeId":"9b25a45e-e7e5-4e06-9929-90e116114919","name":"Guinea","code":"FR","updatedAt":"2020-03-12T00:25:04+03:00"},      
+    //   {"id":"vP38-NdB-S","nodeId":"56294d43-4d8b-45f0-9676-f380930f8d66","name":"Guinea-Bissau","code":"IE","updatedAt":"2020-03-15T00:25:04+03:00"},      
+    //   {"id":"HUZ0tFZw41","nodeId":"dda4c4ae-d2a9-4d91-b36a-1232a399b1f4","name":"Nicaragua","code":"MX","updatedAt":"2020-02-29T00:25:04+03:00"},
+    //   {"id":"o_xKHVYYCb","nodeId":"e7a888bb-420e-4a02-a539-ae29625f77e7","name":"Czech Republic","code":"IE","updatedAt":"2020-03-07T00:25:04+03:00"},
+    //   {"id":"GjmpsFXCnv","nodeId":"7ccf5f7b-a2ba-4bec-baaf-74e68a505110","name":"India","code":"ES","updatedAt":"2020-02-25T00:25:04+03:00"},
+    //   {"id":"HCGgb2ALkw","nodeId":"763ddbb4-b6c8-4646-ba2b-7e7e14412782","name":"Japan","code":"RU","updatedAt":"2020-03-06T00:25:04+03:00"},
+    //   {"id":"G6uw5EaXT8","nodeId":"c21c8574-0d24-4a25-bfa3-d0da3818bfad","name":"Monaco","code":"IN","updatedAt":"2020-03-14T00:25:04+03:00"},
+    //   {"id":"tuMpRj14b3","nodeId":"f2e04466-7ae6-48d4-818a-dc76b9187046","name":"Guadeloupe","code":"MX","updatedAt":"2020-03-09T00:25:04+03:00"},
+    //   {"id":"KF5UhVBanC","nodeId":"4556647a-d090-44a8-81cf-5669c5d691e8","name":"Netherlands Antilles","code":"MX","updatedAt":"2020-03-06T00:25:04+03:00"},
+    //   {"id":"FlSxQ0a3nP","nodeId":"5fbe7b62-fa10-4232-b0d7-b5dd6425b2bf","name":"Myanmar","code":"FR","updatedAt":"2020-03-09T00:25:04+03:00"}
+    // ])
+  })
+})
