8c1a3b1fc417e90d5cf3613d97fc63ef776c038a
LeoVS09 <leovs09@gmail.com>
2020-05-05 20:43:49 +0300

    add tests to aos schema builder

--- tests/unit/lazyDB/readSchema.test.ts
+++ tests/unit/lazyDB/readSchema.test.ts
@@ -23,6 +23,29 @@ describe('AOS builder', () => {
         expect({ isAppended, schema }).toMatchSnapshot()
     })
 
+    it('append id before simple string property', () => {
+        const idField = simplePayload('id')
+        const payload = simplePayload('simple')
+
+        let isAppended = appendPropertyToSchema(schema, idField)
+        expect({ isAppended, schema }).toMatchSnapshot()
+
+        isAppended = appendPropertyToSchema(schema, payload)
+        expect({ isAppended, schema }).toMatchSnapshot()
+    })
+
+    it('append id after simple string property', () => {
+        const payload = simplePayload('simple')
+        const idField = simplePayload('id')
+
+        let isAppended = appendPropertyToSchema(schema, payload)
+        expect({ isAppended, schema }).toMatchSnapshot()
+
+        isAppended = appendPropertyToSchema(schema, idField)
+        expect({ isAppended, schema }).toMatchSnapshot()
+    })
+
+
     it('append multiple string properties', () => {
         const first = simplePayload('first')
         const second = simplePayload('second')
@@ -37,4 +60,53 @@ describe('AOS builder', () => {
         isAppended = appendPropertyToSchema(schema, third)
         expect({ isAppended, schema }).toMatchSnapshot()
     })
+
+    it('append multiple string properties and id', () => {
+        const first = simplePayload('first')
+        const idField = simplePayload('id')
+        const second = simplePayload('second')
+        const nodeId = simplePayload('nodeId')
+        const third = simplePayload('third')
+
+        let isAppended = appendPropertyToSchema(schema, first)
+        expect({ isAppended, schema }).toMatchSnapshot()
+
+        isAppended = appendPropertyToSchema(schema, idField)
+        expect({ isAppended, schema }).toMatchSnapshot()
+
+        isAppended = appendPropertyToSchema(schema, second)
+        expect({ isAppended, schema }).toMatchSnapshot()
+
+        isAppended = appendPropertyToSchema(schema, nodeId)
+        expect({ isAppended, schema }).toMatchSnapshot()
+
+        isAppended = appendPropertyToSchema(schema, third)
+        expect({ isAppended, schema }).toMatchSnapshot()
+    })
+
+    it('append existed simple string property', () => {
+        const payload = simplePayload('simple')
+
+        let isAppended = appendPropertyToSchema(schema, payload)
+        expect({ isAppended, schema }).toMatchSnapshot()
+
+        isAppended = appendPropertyToSchema(schema, payload)
+        expect({ isAppended, schema }).toMatchSnapshot()
+    })
+
+    it('append existed simple string property after not existing', () => {
+        const first = simplePayload('first')
+        const second = simplePayload('second')
+
+        let isAppended = appendPropertyToSchema(schema, first)
+        expect({ isAppended, schema }).toMatchSnapshot()
+
+        isAppended = appendPropertyToSchema(schema, second)
+        expect({ isAppended, schema }).toMatchSnapshot()
+        
+        isAppended = appendPropertyToSchema(schema, first)
+        expect({ isAppended, schema }).toMatchSnapshot()
+    })
+
+
 })
