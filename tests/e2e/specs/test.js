b50414bd63fe0e179a3ffa112368d0d3b25e0df1
LeoVS09 <leovs09@gmail.com>
2020-05-08 21:24:47 +0300

    add simple cypress tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,34 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Main page', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.contains('h1', 'Problems')
+    cy.get('.problems-list').should('be.visible')
+    // TODO: wait until requests is end
+    cy.wait(3000)
+    cy.get('.problems-list .list-item').its('length').should('be.gte', 9)
+    cy.get('.problems-list .list-item').should($div => {
+      const text = $div.text()
+      expect(text.length > 0).to.have.true
+    })
+  })
+})
+
+
+describe('Base UseCase', () => {
+  it('Go to problem', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Problems')
+    cy.get('.problems-list').should('be.visible')
+    cy.wait(3000)
+    cy.get('.problems-list .list-item').its('length').should('be.gte', 9)
+    cy.get('.problems-list .list-item').should($div => {
+      const text = $div.text()
+      expect(text.length > 0).to.have.true
+    })
+    cy.get('.problems-list .list-item').last().click()
+    cy.url().should('contain', 'problems')
+    cy.get('.problem').should('contain', 'Examples')
   })
 })
